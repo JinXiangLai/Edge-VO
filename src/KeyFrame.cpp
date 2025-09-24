624d697e89600e4332ad18ec14db67e77be86e0a
JinXiangLai <3292795816@qq.com>
2025-09-24 18:20:24 +0800

    [Fix] KF1极线方向使用p1->e1的效果，即乘以-1反而看起来好些，极线匹配方向是不是不代表什么？需要对所选方向做进一步研究

--- src/KeyFrame.cpp
+++ src/KeyFrame.cpp
@@ -1269,7 +1269,7 @@ double KeyFrame::FindMatchesWithEpipolarConstraintOnImagePlane(
         return EpipolarMatchType::outOFboundaryORabnormalDepth;
     }
     // 要保证双目图像上极线方向相对于图像是从左到右还是从右到左保持一致
-    //ep1 *= -1;  // 保证是近点指向远点的像素投影坐标，极点对应于近点投影
+    ep1 *= -1;  // 保证是近点指向远点的像素投影坐标，极点对应于近点投影
 
     ep1.normalize();
 
