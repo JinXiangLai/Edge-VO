947b20b65e529564fb650671dc075c29a0caa957
JinXiangLai <3292795816@qq.com>
2025-09-24 23:43:24 +0800

    [Fix] 1. 验证了，不乘以-1时，结果很错误，可视化视频也显示p->e1才对应farPx2->nearPx2 2. 可视化地图点也显示ep1不乘以-1时，结果相比错误得多 3. 注释了3个重要配置参数即：maxDescriptorDist(越小精度越高)，best2SecondRatio(越小歧义越小)，best2SecondDist(越小歧义越小)，值越小均找到越少匹配

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1443,7 +1443,7 @@ Eigen::Vector2d CalculateObvWrtIdepth1Jacobian(const Eigen::Matrix3d& Rc2_c1,
 Eigen::Vector2i ParseKeypointSet(const std::string& s) {
     // 解析"num_num"为数字
     const int _pos = s.find_first_of('_');
-    return {stoi(s.substr(0, _pos)), stoi(s.substr(_pos+1, s.size()))};
+    return {stoi(s.substr(0, _pos)), stoi(s.substr(_pos + 1, s.size()))};
 }
 
 char DrawPerpendicularAndParallelDirectionOFedge(const Mat& edgeImg,
