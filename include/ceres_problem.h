cddfa7ade9dcbd2002142908d3b10837232bfe98
JinXiangLai <3292795816@qq.com>
2025-12-21 17:51:48 +0800

    [Feature] 1.实现基于ceres2对姿态和位置分开求导的流型参数化类 PoseParameterization 2. 实现基于ceres2的投影损失函数ProjectInvDepthResidual

---
+++ include/ceres_problem.h
@@ -0,0 +1,98 @@
+#include <ceres/ceres.h>  // 包含了ceres全部的头文件
+
+#include <Eigen/Dense>
+
+// 自定义四元数的 LocalParameterization
+class PoseParameterization : public ceres::Manifold {
+   public:
+    bool Plus(const double* x, const double* delta,
+              double* x_plus_delta) const override {
+        // 将 delta 转换为四元数
+        Eigen::Matrix<double, 3, 1> delta_q(delta[0], delta[1], delta[2]);
+        double theta = delta_q.norm();
+        Eigen::Quaterniond deltaQ;
+        if (theta > 0.0) {
+            deltaQ =
+                Eigen::Quaterniond(Eigen::AngleAxisd(theta, delta_q / theta));
+        } else {
+            deltaQ = Eigen::Quaterniond(1.0, 0.0, 0.0, 0.0);
+        }
+
+        // 将 x 转换为四元数: qw, qx, qy, qz
+        Eigen::Quaterniond x_q(x[0], x[1], x[2], x[3]);
+
+        // 更新四元数
+        Eigen::Quaterniond x_plus_delta_q = x_q * deltaQ;
+        x_plus_delta_q.normalize();
+
+        // 将结果写回
+        x_plus_delta[0] = x_plus_delta_q.w();
+        x_plus_delta[1] = x_plus_delta_q.x();
+        x_plus_delta[2] = x_plus_delta_q.y();
+        x_plus_delta[3] = x_plus_delta_q.z();
+
+        // 更新3D位置
+        x_plus_delta[4] = x[4] + delta[3];
+        x_plus_delta[5] = x[5] + delta[4];
+        x_plus_delta[6] = x[6] + delta[5];
+
+        return true;
+    }
+
+    virtual bool ComputeJacobian(const double* x, double* jacobian) const {
+        return false;
+    }
+
+    virtual bool RightMultiplyByPlusJacobian(
+        const double* x, const int num_rows, const double* ambient_matrix,
+        double* tangent_matrix) const override {
+        return false;
+    }
+
+    virtual bool Minus(const double* x, const double* y,
+                       double* y_minus_x) const override {
+        return false;
+    }
+
+    virtual bool PlusJacobian(const double* x,
+                              double* jacobian) const override {
+        // 计算的是Plus函数对delta增量的雅可比矩阵
+
+        // 因为我在Evaluate函数中已经直接给出∂e/∂δ，所以这里只需要保证乘上∂e/∂δ值不变即可
+        // 但此时只能使用ceres::LINE_SEARCH，而不能使用ceres::TRUST_REGION
+        // 这里的雅可比行数是7,但是我的目的只需要前6行是单位矩阵即可
+        Eigen::Map<Eigen::Matrix<double, 7, 6, Eigen::RowMajor>> j(jacobian);
+        j.setZero();
+        j.block(0, 0, 6, 6).setIdentity();
+
+        return true;
+    }
+
+    virtual bool MinusJacobian(const double* x,
+                               double* jacobian) const override {
+        return false;
+    }
+
+    int AmbientSize() const override { return 7; }  // Pose的环境维度
+    int TangentSize() const override { return 6; }  // Pose的切空间维度
+};
+
+// 分别代表残差维度，第一个参数块维度Twc1，第2个参数块维度Twc2以及逆深度
+// 旋转使用四元数参数化
+class ProjectInvDepthResidual : public ceres::SizedCostFunction<2, 7, 7, 1> {
+   public:
+    ProjectInvDepthResidual(const Eigen::Vector2d& obvHost,
+                            const Eigen::Vector2d& obvFrame,
+                            const Eigen::Matrix3d& K);
+    ProjectInvDepthResidual(
+        const Eigen::Vector2d& obvHost,
+        const Eigen::Matrix<double, 1, 2, Eigen::RowMajor>& obvFrame,
+        const Eigen::Matrix3d& K);
+    virtual bool Evaluate(double const* const* parameters, double* residuals,
+                          double** jacobians) const override;
+
+   private:
+    Eigen::Vector2d obvHost_;
+    Eigen::Vector2d obvFrame_;
+    Eigen::Matrix3d K_;
+};
---
+++ src/ceres_problem.cpp
@@ -0,0 +1,138 @@
+#include "ceres_problem.h"
+
+#include "Utils.h"
+using namespace std;
+
+ProjectInvDepthResidual::ProjectInvDepthResidual(
+    const Eigen::Vector2d& obvHost, const Eigen::Vector2d& obvFrame,
+    const Eigen::Matrix3d& K)
+    : obvHost_(obvHost), obvFrame_(obvFrame), K_(K) {
+    // 已在继承类指定const，这里就不需要再指定
+}
+
+ProjectInvDepthResidual::ProjectInvDepthResidual(
+    const Eigen::Vector2d& obvHost,
+    const Eigen::Matrix<double, 1, 2, Eigen::RowMajor>& obvFrame,
+    const Eigen::Matrix3d& K)
+    : obvHost_(obvHost), K_(K) {
+    obvFrame_[0] = obvFrame[0];
+    obvFrame_[1] = obvFrame[1];
+}
+
+bool ProjectInvDepthResidual::Evaluate(double const* const* parameters,
+                                       double* residuals,
+                                       double** jacobians) const {
+    // 获取优化参数
+    const double* q_wc1 = parameters[0];  // qw, qx, qy, qz
+    const double* p_wc1 = parameters[0] + 4;
+    const double* q_wc2 = parameters[1];
+    const double* p_wc2 = parameters[1] + 4;
+
+    const Eigen::Quaterniond Qwc1(q_wc1[0], q_wc1[1], q_wc1[2], q_wc1[3]);
+    const Eigen::Vector3d Pwc1(p_wc1[0], p_wc1[1], p_wc1[2]);
+    const Eigen::Quaterniond Qwc2(q_wc2[0], q_wc2[1], q_wc2[2], q_wc2[3]);
+    const Eigen::Vector3d Pwc2(p_wc2[0], p_wc2[1], p_wc2[2]);
+    const double& invZ1 = *parameters[2];
+    const double z1 = 1.0 / invZ1;
+
+    // 重投影
+    const Eigen::Vector3d pc1Norm((obvHost_.x() - K_(0, 2)) / K_(0, 0),
+                                  (obvHost_.y() - K_(1, 2)) / K_(1, 1), 1.0);
+    const Eigen::Vector3d pc1 = z1 * pc1Norm;
+    const Eigen::Vector3d pw = Qwc1 * pc1 + Pwc1;
+    const Eigen::Vector3d dPw = pw - Pwc2;
+    const Eigen::Quaterniond Qc2w = Qwc2.inverse();
+    const Eigen::Vector3d pc2 = Qc2w * dPw;
+    const Eigen::Vector3d pc2Norm = pc2 / pc2.z();
+    const Eigen::Vector2d projectObv2(pc2Norm.x() * K_(0, 0) + K_(0, 2),
+                                      pc2Norm.y() * K_(1, 1) + K_(1, 2));
+
+    // 记录残差
+    residuals[0] = projectObv2[0] - obvFrame_[0];
+    residuals[1] = projectObv2[1] - obvFrame_[1];
+
+    // 计算雅可比
+    if (jacobians) {
+        // clang-format off
+            /******** 投影过程 ********
+            * K.inv * (u1, v1, 1) --> Pc1_norm * z1 --> Twc1 * Pc1 --> Twc2.inv * Pw -->  
+            *  Pc2 / z2 -> K * Pc2_norm -> (u2, v2, 1) -> res(u2, v2)
+            *
+            * res w.r.t (u2, v2) [1x2]
+            * (u2, v2) w.r.t Pc2_norm [2x3]
+            * Pc2_norm w.r.t Pc2 [3x3]
+            * Pc2 w.r.t Twc2 [3x6] ------> optimization variable
+            * Pc2 w.r.t Pw [3x3]
+            * Pw w.r.t Twc1 [3x6] -------> optimization variable
+            * Pw w.r.t Pc1
+            * Pc1 w.r.t z1 [3x1] --------> optimization variable
+            ************************/
+        // clang-format on
+        const auto& J_px2_Pc2Norm =
+            K_.block(0, 0, 2, 3);  // 第3列可不置0,因为后续J_Pc2Norm_Pc2第3行为0
+
+        Eigen::Matrix<double, 3, 3> J_Pc2Norm_Pc2;
+        const double d = 1 / pc2.z();
+        const double d2 = d * d;
+        J_Pc2Norm_Pc2 << d, 0, -pc2.x() * d2, 0, d, -pc2.y() * d2, 0, 0, 0;
+
+        const Eigen::Matrix<double, 2, 3> J_px2_Pc2 =
+            J_px2_Pc2Norm * J_Pc2Norm_Pc2;
+        const Eigen::Matrix<double, 2, 3>& J_res_Pc2 = J_px2_Pc2;
+
+        Eigen::Matrix<double, 3, 6> J_Pc2_Twc2;
+        // Pc2 w.r.t Rwc2
+        J_Pc2_Twc2.block<3, 3>(0, 0) = SkewSymmetric(Qc2w * dPw);
+        // Pc2 w.r.t Pwc2
+        const Eigen::Matrix3d Rc2w = Qc2w.toRotationMatrix();
+        J_Pc2_Twc2.block<3, 3>(0, 3) = -Rc2w;
+
+        // Pc2 w.r.t Pw
+        const Eigen::Matrix3d& J_Pc2_Pw = Rc2w;
+
+        Eigen::Matrix<double, 3, 6> J_Pw_Twc1;
+        const Eigen::Matrix3d Rwc1 = Qwc1.toRotationMatrix();
+        // Pw w.r.t Twc1 : Pw = Twc1 * Pc1 = Rwc1 * pc1 + Pwc1
+        // Pw w.r.t Rwc1
+        J_Pw_Twc1.block<3, 3>(0, 0).noalias() = -Rwc1 * SkewSymmetric(pc1);
+
+        // Pw w.r.t Pwc1
+        J_Pw_Twc1.block<3, 3>(0, 3) = Eigen::Matrix3d::Identity();
+
+        // Pw w.r.t Pc1
+        const Eigen::Matrix3d& J_Pw_Pc1 = Rwc1;
+
+        // Pc1 w.r.t z
+        // 使用逆深度表示
+        const double invZ1Square = invZ1 * invZ1;
+        Eigen::Vector3d J_Pc1_z;
+        J_Pc1_z << -pc1Norm.x() / invZ1Square, -pc1Norm.y() / invZ1Square,
+            -1 / invZ1Square;
+
+        const Eigen::Matrix<double, 3, 1> J_Pw_z = J_Pw_Pc1 * J_Pc1_z;
+
+        // Residual w.r.t optimization variables Jacobian
+        if (jacobians[0]) {
+            Eigen::Map<Eigen::Matrix<double, 2, 7, Eigen::RowMajor>> J_res_Twc1(
+                jacobians[0]);
+            // 由于维度是关于四元数的，所以需要设置zero
+            J_res_Twc1.setZero();
+            J_res_Twc1.block<2, 6>(0, 0) = J_res_Pc2 * J_Pc2_Pw * J_Pw_Twc1;
+        }
+
+        if (jacobians[1]) {
+            Eigen::Map<Eigen::Matrix<double, 2, 7, Eigen::RowMajor>> J_res_Twc2(
+                jacobians[1]);
+            J_res_Twc2.setZero();
+            J_res_Twc2.block<2, 6>(0, 0) = J_res_Pc2 * J_Pc2_Twc2;
+        }
+
+        if (jacobians[2]) {
+            Eigen::Map<Eigen::Matrix<double, 2, 1>> J_res_invZ1(jacobians[2]);
+            J_res_invZ1.setZero();
+            J_res_invZ1 = J_res_Pc2 * J_Pc2_Pw * J_Pw_z;
+        }
+    }
+
+    return true;
+}
